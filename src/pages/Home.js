c420482d2a3dfe648c7a0a6e765b448c0bfc26a6
Wollivan <tim.smith2814@gmail.com>
2022-12-22 19:41:50 +0000

    Git gud

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Table from "../components/Table/Table";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>This is the home page</h2>
+      <p>I'm feeling very clever</p>
+      <Table />
+      <Table />
+      <Table />
+    </div>
+  );
+}
